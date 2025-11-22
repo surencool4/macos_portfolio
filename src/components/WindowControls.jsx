0496ac96214f799ff972eb7a34c980613fbf68d0
Surendra Budhathoki <surendra@pahadi.net>
2025-11-22 19:01:31 +0545

    Implement high order component and store

---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,12 @@
+import useWindowStore from "#store//window.js";
+
+const WindowControls = ({target}) => {
+    const {closeWindow} = useWindowStore();
+
+    return <div id="window-controls">
+        <div className="close" onClick={() => closeWindow(target)}></div>
+        <div className="minimize"></div>
+        <div className="maximize"></div>
+    </div>
+}
+export default WindowControls
